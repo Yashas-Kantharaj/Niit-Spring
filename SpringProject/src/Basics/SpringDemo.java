16aea48015aaf8ee39791768bb45fbdaa7383523
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-19 12:04:30 +0530

    init

---
+++ SpringProject/src/Basics/SpringDemo.java
@@ -0,0 +1,19 @@
+package Basics;
+
+
+	public class SpringDemo {  
+		private String name;  
+		  
+		public String getName() {  
+		    return name;  
+		}  
+		  
+		public void setName(String name) {  
+		    this.name = name;  
+		}  
+		  
+		public void displayInfo(){  
+		    System.out.println("Hello: "+name);  
+		}  
+	}  
+
