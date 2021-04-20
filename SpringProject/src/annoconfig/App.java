8c6950a7a6606579897e313aa370a8c440af3122
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-20 12:32:44 +0530

    annoconfig

---
+++ SpringProject/src/annoconfig/App.java
@@ -0,0 +1,13 @@
+package annoconfig;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		
+		ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+		Samsung s8 = factory.getBean(Samsung.class);
+		s8.config();
+	}
+}
---
+++ SpringProject/src/annoconfig/Samsung.java
@@ -0,0 +1,7 @@
+package annoconfig;
+
+public class Samsung {
+	public void config() {
+		System.out.println("octa core-- 8 gb");
+	}
+}
