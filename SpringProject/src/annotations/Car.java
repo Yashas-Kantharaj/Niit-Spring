3d533f3ee8289b0560498d6a28510fc0f1491550
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-20 12:13:23 +0530

    auto wiring

--- SpringProject/src/annotations/Car.java
+++ SpringProject/src/annotations/Car.java
@@ -1,10 +1,26 @@
 package annotations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Car implements Vehicle {
-	public void  drive() {
-		System.out.println("you are driving car");
+	
+	@Autowired
+	Tyre tyre;
+	
+	
+	public Tyre getTyre() {
+		return tyre;
+	}
+
+
+	public void setTyre(Tyre tyre) {
+		this.tyre = tyre;
+	}
+
+
+	public void drive() {
+		System.out.println("driving car with tyre"+tyre);
 	}
 }
