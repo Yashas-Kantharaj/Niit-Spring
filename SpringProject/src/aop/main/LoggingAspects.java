5f14daddb695056e3eaf83b8fb88ecc605ed80cd
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-21 10:52:47 +0530

    pointcut

--- SpringProject/src/aop/main/LoggingAspects.java
+++ SpringProject/src/aop/main/LoggingAspects.java
@@ -2,12 +2,15 @@ package aop.main;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 public class LoggingAspects {
-	@Before("execution(* aop.main..*.*(..))")
+	@Before("allGetters()")
 	public void loggingAdvice() {
 		System.out.println("writing log before method is executed");
 	}
+	@Pointcut("execution(* aop..*.get*())")
+	public void allGetters() {}
 }
