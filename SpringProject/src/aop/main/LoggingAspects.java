b4d6639e782bc6472e850cd498a6783f8b33a089
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-22 10:40:44 +0530

    xml aop

--- SpringProject/src/aop/main/LoggingAspects.java
+++ SpringProject/src/aop/main/LoggingAspects.java
@@ -7,20 +7,20 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
-@Aspect
+
 public class LoggingAspects {
 //	@Before("allCircleMethods()")
 //	public void loggingAdvice() {
 //		System.out.println("writing log before method is executed");
 //	}
-//	@Pointcut("execution(* aop..*.get*())")
-//	public void allGetters() {}
+	@Pointcut("execution(* aop..*.get*())")
+	public void allGetters() {}
 //	
 //	@Pointcut("within(aop.model.Circle)")
 //	public void allCircleMethods() {}
 	
 	
-	@Around("@annotation(aop.main.Loggable)") 
+
 	public void myAroundAdvice(ProceedingJoinPoint pjp) {
 		
 		try {
