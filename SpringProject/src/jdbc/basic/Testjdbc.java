a9f82d7fac1e9edceb1d663f80c226d2d390ceb3
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-23 10:07:42 +0530

    spring jdbc using xml

--- SpringProject/src/jdbc/basic/Testjdbc.java
+++ SpringProject/src/jdbc/basic/Testjdbc.java
@@ -1,13 +1,17 @@
 package jdbc.basic;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import jdbc.basic.dao.StudentDao;
 import jdbc.basic.dao.StudentDaoImpl;
 import jdbc.basic.model.Student;
 
 public class Testjdbc {
 	public static void main(String[] args) {
-		Student s1= new Student(4, "ramya", 8, 60);
-		StudentDao studentDao = new StudentDaoImpl();
+		ApplicationContext context = new ClassPathXmlApplicationContext("jdbc.xml");
+		Student s1= new Student(5, "rakesh", 8, 67);
+		StudentDao studentDao = (StudentDao) context.getBean("studentDao");
 		studentDao.insert(s1);
 
 		
--- SpringProject/src/jdbc/basic/dao/StudentDaoImpl.java
+++ SpringProject/src/jdbc/basic/dao/StudentDaoImpl.java
@@ -4,31 +4,40 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Repository;
 
 import jdbc.basic.model.Student;
 
+@Repository("studentDoa")
 public class StudentDaoImpl  implements StudentDao{
-	private JdbcTemplate jt= new JdbcTemplate(getDataSource());
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate ;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
 	
 	@Override
 	public void insert(Student s) {
 		String sql = "insert into student values (?,?,?,?)";
 		Object[] objects= {s.getId(),s.getName(),s.getSem(),s.getAverage()};
 
-		int no_rows_inserted =	jt.update(sql,objects);
+		int no_rows_inserted =	jdbcTemplate.update(sql,objects);
 		System.out.println("no of rows inserted  is"+ no_rows_inserted);
 	}
 	
 	
-	public  DataSource getDataSource() {
-		String url ="jdbc:mysql://localhost/spring_jdbc";
-		String userid = "root";
-		String pwd = "";
-		DriverManagerDataSource ds = new DriverManagerDataSource(url, userid, pwd);
-		System.out.println("Database is connected !");
-		return ds;
-		
-	}
+//	public  DataSource getDataSource() {
+//		String url ="jdbc:mysql://localhost/spring_jdbc";
+//		String userid = "root";
+//		String pwd = "";
+//		DriverManagerDataSource ds = new DriverManagerDataSource(url, userid, pwd);
+//		System.out.println("Database is connected !");
+//		return ds;
+//		
+//	}
 }
