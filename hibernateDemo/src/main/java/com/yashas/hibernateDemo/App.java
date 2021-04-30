b9d80c416291b9104a7d403fd8f9333da3ab5a83
Yashas-Kantharaj <yashastkd@gmail.com>
2021-04-30 12:26:49 +0530

    show sql

--- hibernateDemo/src/main/java/com/yashas/hibernateDemo/App.java
+++ hibernateDemo/src/main/java/com/yashas/hibernateDemo/App.java
@@ -5,16 +5,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        Student student = new Student(212, "yashas", 1, 55);
+        Student student = new Student(213, "yashaswini", 1, 66);
         Configuration configuration = new Configuration().configure().addAnnotatedClass(Student.class);
         
         SessionFactory sessionFactory = configuration.buildSessionFactory();
